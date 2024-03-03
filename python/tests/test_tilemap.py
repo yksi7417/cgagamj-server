5f3331cd75b759b09ffe95c3c37809ec13f34957
Anthony Si <yatkasi@gmail.com>
2024-03-03 16:57:26 -0500

    seems to have correct data being passed after using ubyte

--- python/tests/test_tilemap.py
+++ python/tests/test_tilemap.py
@@ -1,5 +1,5 @@
 import pytest
-from src.common.tilemap import add
+from src.common.tilemap import add, get_randomized_full_size
 
 def test_add():
     assert add(2, 3) == 5
@@ -7,6 +7,8 @@ def test_add():
     assert add(0, 0) == 0
 
 def test_full_set():
-    full_set = byte[144] // Cantonese MJ has 144 tiles 
-    assert len(full_set) == 144 
-    
+    full_set = get_randomized_full_size()  
+    assert len(full_set) == 144
+    assert sum(full_set) ==  4 * sum(range(36)) 
+
+
