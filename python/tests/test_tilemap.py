deca76f77c3fd94f772d3438445ede359765dc00
Anthony Si <yatkasi@gmail.com>
2024-03-03 15:16:18 -0500

    a tilemap has 144

--- python/tests/test_tilemap.py
+++ python/tests/test_tilemap.py
@@ -5,3 +5,8 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
+def test_full_set():
+    full_set = byte[144] // Cantonese MJ has 144 tiles 
+    assert len(full_set) == 144 
+    
