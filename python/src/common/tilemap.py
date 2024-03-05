15dd85f7aa0485ac51a4885116873369c4906b4e
Anthony Si <yatkasi@gmail.com>
2024-03-04 19:21:26 -0500

    update the tiles to use number from 0-143

--- python/src/common/tilemap.py
+++ python/src/common/tilemap.py
@@ -2,8 +2,7 @@ import random
 
 def get_randomized_full_set():
     unique_tiles=[]
-    for i in range(4):
-        unique_tiles.extend(list(range(36)))
+    unique_tiles.extend(list(range(144)))
     random.shuffle(unique_tiles)
     return unique_tiles
 
--- python/tests/test_tilemap.py
+++ python/tests/test_tilemap.py
@@ -4,6 +4,6 @@ from src.common.tilemap import get_randomized_full_set
 def test_full_set():
     full_set = get_randomized_full_set()  
     assert len(full_set) == 144
-    assert sum(full_set) ==  4 * sum(range(36)) 
+    assert sum(full_set) ==  10296
 
 
