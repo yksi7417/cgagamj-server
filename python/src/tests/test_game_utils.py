09f7976a3158b0b1269e69a6a25443499cf63b61
Anthony Si <yatkasi@gmail.com>
2024-03-04 21:35:52 -0500

    remove src as it's not needed

--- python/src/tests/test_game_utils.py
+++ python/src/tests/test_game_utils.py
@@ -1,6 +1,6 @@
 import pytest
 import flatbuffers
-from src.common.game_utils import new_game, print_game, decode_game, encode_game
+from common.game_utils import new_game, print_game, decode_game, encode_game
 
 def test_encode_decode_print():
     builder = flatbuffers.Builder(1024)
